29d5df4cfeccc5c6813101351148e28169e53a91
olliekennedy <ollie.kennedy1@gmail.com>
2020-10-02 15:29:36 +0100

    random code storage created

---
+++ random_code_storage.rb
@@ -0,0 +1,10 @@
+
+
+dog_walk = Todo.new("Walk the dog")
+sweep = Todo.new("Sweep the patio")
+hoover = Todo.new("Hoover the lounge")
+
+main_list = TodoList.new
+main_list.add(dog_walk)
+main_list.add(hoover)
+main_list.print
