b4a81ac78410e13669506ea70c0fc87a420926e5
olliekennedy <ollie.kennedy1@gmail.com>
2020-10-20 12:19:08 +0100

    Working but inefficient still - halfway through trying to get binary counting working

--- codewars/lib/balanced_parentheses.rb
+++ codewars/lib/balanced_parentheses.rb
@@ -1,17 +1,27 @@
 class Body
   def balanced_parens(n)
+    max = ('1' * n*2).to_i(2)
+
+    permas = (0..max).map do |i|
+      i.to_s(2).rjust(n).gsub(" ","0")
+    end
+    puts permas
+
     return [""] if n == 0
-    all = ([-1,1]*n).permutation.to_a
+    sample = ([-1, 1] * (n - 1)).push(-1)
+    puts "before perms"
+    all = sample.permutation.to_a.uniq
+    puts "after perms"
     valid_perms = []
     all.each do |perm|
+      perm.unshift(1)
       cnt = 0
       perm.each do |x|
-        cnt += x.to_i
+        cnt += x
         break if cnt == -1
       end
       valid_perms << perm if cnt == 0
     end
-    valid_perms.uniq!
     valid_parens = []
     valid_perms.each do |perm|
       valid_paren = []
--- codewars/spec/balanced_parentheses_spec.rb
+++ codewars/spec/balanced_parentheses_spec.rb
@@ -2,11 +2,14 @@ require 'balanced_parentheses'
 
 RSpec.describe Body do
   describe '#balanced_parens' do
-    it 'returns correct for 3' do
-      expect(subject.balanced_parens(3)).to eq ["((()))","(()())","(())()","()(())", "()()()"]
-    end
-    it 'returns correct for 2' do
-      expect(subject.balanced_parens(2)).to eq ["()()","(())"].sort
+    # it 'returns correct for 3' do
+    #   expect(subject.balanced_parens(3)).to eq ["((()))","(()())","(())()","()(())", "()()()"]
+    # end
+    # it 'returns correct for 2' do
+    #   expect(subject.balanced_parens(2)).to eq ["()()","(())"].sort
+    # end
+    it 'returns 5' do
+      subject.balanced_parens(5)
     end
   end
 end
